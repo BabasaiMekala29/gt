7935b57168fc6445988f8749e75f4f1e8acceae7
BabasaiMekala29 <110708314+BabasaiMekala29@users.noreply.github.com>
2024-06-15 22:07:16 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,10 +9,31 @@ const jwt = require('jsonwebtoken');
 const app = express();
 
 // middleware
+// app.use(cors());
+// // app.use(cors({credentials:true, origin: 'https://glidethrough-frontend.vercel.app'}));
+// app.use((req, res, next) => {
+  
+//   res.setHeader('Access-Control-Allow-Origin', 'https://glidethrough-frontend.vercel.app'); // Change this to your frontend domain
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow credentials (e.g., cookies)
+//   next();
+// });
+app.use(cors({
+  origin: 'https://glidethrough-frontend.vercel.app',
+  methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders: ['X-CSRF-Token', 'X-Requested-With', 'Accept', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Content-Type', 'Date', 'X-Api-Version'],
+  credentials: true
+}));
+
 app.use(express.static('public'));
 app.use(cookieParser());
-app.use(cors({credentials:true, origin: 'http://localhost:3000'}));
 app.use(express.json());
+
+
+
+
+
 
 
 const dbURI = process.env.DB_URL;
@@ -21,7 +42,5 @@ mongoose.connect(dbURI)
   .catch((err) => console.log(err));
 
 // app.get('*', checkUser);
-
-
 
 app.use(webRoutes);
